7aaa6db31dbda468820eb4d525b190bbf17e1c72
notlee <lee.moody@ft.com>
2019-04-08 13:05:27 +0100

    Use ESM instead of CJS.

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -1,4 +1,4 @@
-require('../../main');
+import * as oOverlay from '../../main';
 
 document.addEventListener("DOMContentLoaded", function() {
 	document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
