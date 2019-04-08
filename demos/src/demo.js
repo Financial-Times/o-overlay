34257a68f9afe721017e6df0fc54150592dc93b4
notlee <lee.moody@ft.com>
2019-04-08 17:38:38 +0100

    Demo lint

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -1,4 +1,4 @@
-import oOverlay from '../../main';
+import oOverlay from '../../main'; //eslint-disable-line no-unused-vars
 
 document.addEventListener("DOMContentLoaded", function() {
 	document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
