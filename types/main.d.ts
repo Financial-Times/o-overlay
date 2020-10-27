f70bec2beae921ccfa4b885d697ea081ae763f60
Jake Champion <me@jakechampion.name>
2020-10-27 16:32:02 +0000

    add licence, type, export-map, and type definitions

---
+++ types/main.d.ts
@@ -0,0 +1,2 @@
+export default Overlay;
+import Overlay from "./src/js/overlay.js";
---
+++ types/src/js/utils.d.ts
@@ -0,0 +1,14 @@
+declare namespace _default {
+    function unCapitalise(str: any): any;
+    function capitalise(str: any): any;
+    function copyContentFromElement(content: any, callback: any): void;
+    function copyContentFromUrl(url: any, callback: any): void;
+    function optionsFromKey(key: any, value: any, opts: any): any;
+    function getOffsetRect(e: any): {
+        width: any;
+        height: any;
+        top: number;
+        left: number;
+    };
+}
+export default _default;
